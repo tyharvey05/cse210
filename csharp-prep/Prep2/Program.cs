81ded5430d9ba8d2776cb00470ebc4d3dd3f007f
tyharvey05 <p1n3xiv@gmail.com>
2025-01-09 12:40:47 -0700

    added code for 'C# Prep 2'

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?: ");
+        string userResponse = Console.ReadLine();
+        int gradePercentage = int.Parse(userResponse);
+        string letter = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}.");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You have passed the class. Congrats!");
+        }
+        else
+        {
+            Console.WriteLine("You have not passed the class.");
+        }
     }
 }
