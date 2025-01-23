7920634801fe9187749a7698271911287667b50f
tyharvey05 <p1n3xiv@gmail.com>
2025-01-23 11:09:57 -0700

    added code for 'W03 Prepare'

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Developer";
+        job1._startYear = 2015;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Software Engineer";
+        job2._startYear = 2020;
+        job2._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "Ty Harvey";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
