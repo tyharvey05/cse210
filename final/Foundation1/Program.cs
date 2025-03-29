8a4d2bf0c8ee523a361cbb233cf2a75900cd2e1a
tyharvey05 <p1n3xiv@gmail.com>
2025-03-28 22:19:34 -0600

    foundations 1 and 2 updated

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,70 @@
 using System;
+using System.Collections.Generic;
+
+class Video
+{
+    private string _title;
+    private string _author;
+    private int _length;
+    private List<Comment> _comments;
+
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+        _comments = new List<Comment>();
+    }
+
+    public string GetTitle() => _title;
+    public string GetAuthor() => _author;
+    public int GetLength() => _length;
+
+    public void AddComment(string commenterName, string text)
+    {
+        _comments.Add(new Comment(commenterName, text));
+    }
+
+    public int GetNumberOfComments() => _comments.Count;
+
+    public List<Comment> GetComments() => _comments;
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"Author: {_author}");
+        Console.WriteLine($"Length: {_length} minutes");
+        Console.WriteLine($"Number of Comments: {GetNumberOfComments()}");
+        Console.WriteLine("Comments:");
+        foreach (var comment in _comments)
+        {
+            Console.WriteLine($"- {comment.GetCommenterName()}: {comment.GetText()}");
+        }
+    }
+}
+
+class Comment
+{
+    private string _commenterName;
+    private string _text;
+
+    public Comment(string commenterName, string text)
+    {
+        _commenterName = commenterName;
+        _text = text;
+    }
+
+    public string GetCommenterName() => _commenterName;
+    public string GetText() => _text;
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Video video1 = new Video("Introduction to C#", "John Doe", 10);
+        video1.AddComment("Alice", "Great explanation!");
+        video1.AddComment("Bob", "Very helpful, thanks!");
+        video1.DisplayInfo();
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,90 @@
 using System;
 
+class Product
+{
+    private string _name;
+    private double _price;
+
+    public Product(string name, double price)
+    {
+        _name = name;
+        _price = price;
+    }
+
+    public string GetName() => _name;
+    public double GetPrice() => _price;
+}
+
+class Address
+{
+    private string _street;
+    private string _city;
+    private string _zipCode;
+
+    public Address(string street, string city, string zipCode)
+    {
+        _street = street;
+        _city = city;
+        _zipCode = zipCode;
+    }
+
+    public string GetFullAddress() => $"{_street}, {_city}, {_zipCode}";
+}
+
+class Customer
+{
+    private string _name;
+    private Address _address;
+
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
+
+    public string GetName() => _name;
+    public Address GetAddress() => _address;
+}
+
+class Order
+{
+    private Customer _customer;
+    private Product _product;
+    private int _quantity;
+    private double _totalPrice;
+
+    public Order(Customer customer, Product product, int quantity)
+    {
+        _customer = customer;
+        _product = product;
+        _quantity = quantity;
+        CalculateTotalPrice();
+    }
+
+    private void CalculateTotalPrice()
+    {
+        _totalPrice = _product.GetPrice() * _quantity;
+    }
+
+    public string GetOrderDetails()
+    {
+        return $"Customer: {_customer.GetName()}\n" +
+               $"Address: {_customer.GetAddress().GetFullAddress()}\n" +
+               $"Product: {_product.GetName()}\n" +
+               $"Quantity: {_quantity}\n" +
+               $"Total Price: ${_totalPrice:F2}";
+    }
+}
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Address address = new Address("123 Main St", "Springfield", "12345");
+        Customer customer = new Customer("Alice", address);
+        Product product = new Product("Laptop", 999.99);
+        Order order = new Order(customer, product, 2);
+
+        Console.WriteLine(order.GetOrderDetails());
     }
 }
